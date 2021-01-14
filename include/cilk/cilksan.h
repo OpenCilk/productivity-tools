d125ee86180778d09c3f5f3935a81da81d3efc8a
TB Schardl <neboat@mit.edu>
2021-01-14 05:03:11 +0000

    [cilksan] Draft external API for Cilksan fake locks.

--- include/cilk/cilksan.h
+++ include/cilk/cilksan.h
@@ -16,12 +16,21 @@
 
 #endif // #ifdef __cplusplus
 
-
 #ifdef __cilksan__
 
 CILKSAN_EXTERN_C void __cilksan_enable_checking(void) CILKSAN_NOTHROW;
 CILKSAN_EXTERN_C void __cilksan_disable_checking(void) CILKSAN_NOTHROW;
 CILKSAN_EXTERN_C bool __cilksan_is_checking_enabled(void) CILKSAN_NOTHROW;
+
+CILKSAN_EXTERN_C void __cilksan_acquire_lock(const void *mutex) CILKSAN_NOTHROW;
+CILKSAN_EXTERN_C void __cilksan_release_lock(const void *mutex) CILKSAN_NOTHROW;
+CILKSAN_EXTERN_C void __cilksan_begin_atomic() CILKSAN_NOTHROW;
+CILKSAN_EXTERN_C void __cilksan_end_atomic() CILKSAN_NOTHROW;
+
+CILKSAN_EXTERN_C void
+__cilksan_register_lock_explicit(const void *mutex) CILKSAN_NOTHROW;
+CILKSAN_EXTERN_C void
+__cilksan_unregister_lock_explicit(const void *mutex) CILKSAN_NOTHROW;
 
 #else // #ifdef __cilksan__
 
@@ -29,6 +38,48 @@ CILKSAN_EXTERN_C void __cilksan_enable_checking(void) CILKSAN_NOTHROW {}
 CILKSAN_EXTERN_C void __cilksan_disable_checking(void) CILKSAN_NOTHROW {}
 CILKSAN_EXTERN_C bool __cilksan_is_checking_enabled(void) { return false; }
 
+CILKSAN_EXTERN_C void
+__cilksan_acquire_lock(const void *mutex) CILKSAN_NOTHROW {}
+CILKSAN_EXTERN_C void
+__cilksan_release_lock(const void *mutex) CILKSAN_NOTHROW {}
+CILKSAN_EXTERN_C void __cilksan_begin_atomic() CILKSAN_NOTHROW {}
+CILKSAN_EXTERN_C void __cilksan_end_atomic() CILKSAN_NOTHROW {}
+
+CILKSAN_EXTERN_C void
+__cilksan_register_lock_explicit(const void *mutex) CILKSAN_NOTHROW {}
+CILKSAN_EXTERN_C void
+__cilksan_unregister_lock_explicit(const void *mutex) CILKSAN_NOTHROW {}
+
 #endif // #ifdef __cilksan__
+
+typedef struct Cilksan_fake_mutex {
+  int fake_mutex;
+
+#ifdef __cplusplus
+  Cilksan_fake_mutex() {
+    __cilksan_register_lock_explicit(this);
+  }
+  ~Cilksan_fake_mutex() {
+    __cilksan_unregister_lock_explicit(this);
+  }
+#endif
+} Cilksan_fake_mutex;
+
+#ifdef __cplusplus
+
+class Cilksan_fake_lock_guard {
+  const void *_mutex;
+  Cilksan_fake_lock_guard() = delete;
+
+public:
+  Cilksan_fake_lock_guard(const void *mutex) : _mutex(mutex) {
+    __cilksan_acquire_lock(_mutex);
+  }
+  ~Cilksan_fake_lock_guard() {
+    __cilksan_release_lock(_mutex);
+  }
+};
+
+#endif
 
 #endif // INCLUDED_CILK_CILKSAN_H
